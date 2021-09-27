cbd8042a9d7638daafabd6986f0153c3ce4a7839
barbara <barbara165@mail.ru>
2021-09-27 16:04:26 +0300

    DZ_4

---
+++ DZ_4/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define("IMG_SMALL","gallery_img/small/");
+define("IMG_BIG","gallery_img/big/");
+define('TEMPLATES_DIR', 'templates/');
+define('LAYOUTS_DIR', 'layouts/');
+define("IMG_NEW","gallery_img/newImages/");
+
+include "download_img.php";
